6e35e19524df6d785619c470968e6862ecee503f
hikio-17 <fajri010217@gmail.com>
2023-08-22 17:45:24 +0700

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import Canvas from './canvas'
-import Customizer from './pages/Customizer'
-import Home from './pages/Home'
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
-function App () {
+function App() {
   return (
-    <main className='app transition-all ease-in'>
+    <main className="app transition-all ease-in">
       <Home />
       <Canvas />
       <Customizer />
